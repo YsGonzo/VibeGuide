ca893ed1ba77dd77b3b752f1476bd4ad78eca844
Jayson Gonzalez <89937039+YsGonzo@users.noreply.github.com>
2023-02-21 20:19:32 -0500

    Delete LiVibes directory

--- LiVibes/offline.php
+++
@@ -1,7 +0,0 @@
-<?php
-// offline.php - My Twitter Website Offline
-// Written by:  Charles Kaplan, December 2017
-
-	echo "<br>&nbsp;&nbsp;$desc_short is off-line for maintenance, please try again later.<br><br>"
-
-?>
--- LiVibes/verify_logon.php
+++
@@ -1,9 +0,0 @@
-<?php
-// verify_logon.php - Verify Logon
-// Written by: Charles Kaplan, December 2017
-
-  if (!$logon) {
-      	header('Location: website.php'); 
-	exit;
-	}
-?>
